41c7862284516dca69f2a862e8a6e2db50fd240e
CoreNex17 <draculaz120@gmail.com>
2025-06-19 19:51:39 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,26 @@
+
+ #task 1
+
+a=input("enter your number :")
+a=float(a)
+b=input("enter your number :")
+b=float(b)
+print('addition =',a+b)
+print('subtraction =',a-b)
+print('multiplication =',a*b)
+print('division =',a/b)
+
+
+#task 2
+
+a=input('enter your first name')
+b=input(' enter your last name')
+full_name = f"{a} {b}"
+print(f"hello, {full_name}! welcome to python program.")
+
+
+
+
+
+
+

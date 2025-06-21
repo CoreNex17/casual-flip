53311499fb33b37782a455afd8a156eda2d8eb5d
CoreNex17 <draculaz120@gmail.com>
2025-06-22 00:14:04 +0530

    Add files via upload

---
+++ ASSIGNMENT_2.py
@@ -0,0 +1,36 @@
+
+#task 1
+
+count = 1
+while count <=26:
+    print(count)
+    count= count + 2
+
+a=int(input("enter the number"))
+if a %2 == 0:
+    print( a, 'is an even number')
+
+else :
+    print(a ,'is a odd number')
+
+
+
+#task 2
+total=0
+for i in range(1,51):
+    total += i
+    print('the sum of numbers from 1 to 50 is ',total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
